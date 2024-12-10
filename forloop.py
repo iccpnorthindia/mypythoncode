8036e999d080b0a7c4d8e93bb53254af8f2a88ab
gautam <gautam@gmail.com>
2024-12-10 11:46:05 +0530

    this is my for loop code

---
+++ forloop.py
@@ -0,0 +1,28 @@
+for india in range (6):     # show output  0,1,2,3,4,5
+    print(india)
+    print()
+
+for india in range(1,6):  # show output 1,2,3,4,5
+    print(india)
+    print()
+
+for india in range(1,6,2):  # show output 1,3,5
+    print(india)
+    print()
+
+for india in range(1,6):  # show output 1,2,3,4,5
+    print("iccp")
+    print()
+
+for india in range(1,11):  # show output 1,2,3,4,5
+        print("iccp")
+
+for india in range(1,11):  # show output 1,2,3,4,5
+        print("2*iccp")
+
+for iccp in range(1,11):  # show output 1,2,3,4,5
+        print("10*",iccp,"=",10*iccp)
+
+for rn in range(10,0,-2):  # show output 1,2,3,4,5
+    print(rn)
+
